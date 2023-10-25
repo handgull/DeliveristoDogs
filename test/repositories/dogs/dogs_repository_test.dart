3824a8df143f0408fd899416e6aae462f0cc6445
handgull <gullandrea693@gmail.com>
2023-10-25 13:16:07 +0200

    dogs repository tests with a good coverage

--- test/repositories/dogs/dogs_repository_test.dart
+++ test/repositories/dogs/dogs_repository_test.dart
@@ -9,6 +9,7 @@ import 'package:deliveristo_dogs/repositories/dogs_repository.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
+import '../../fixtures/models/dog_pic_fixture_factory.dart';
 import 'dogs_repository_test.mocks.dart';
 
 /// Test case for the class DogsRepositoryImpl
@@ -30,7 +31,7 @@ void main() {
     );
   });
 
-  group('description', () {
+  group('dogs repository randomByBreed', () {
     late String message;
     late String status;
     late DogPic random;
@@ -42,12 +43,12 @@ void main() {
       random = DogPic(message);
       breed = faker.randomGenerator.string(10);
     });
-    test('getting random successfully', () async {
+    test('getting random by breed successfully', () async {
       when(dogsService.randomByBreed(breed)).thenAnswer((_) async => Response(
             message: message,
             status: status,
           ));
-      when(randomMapper.from(message)).thenAnswer((_) => random);
+      when(randomMapper.from(message)).thenReturn(random);
 
       final result = await repository.randomByBreed(breed);
 
@@ -57,7 +58,7 @@ void main() {
       verify(randomMapper.from(message)).called(1);
     });
 
-    test('getting random with error', () async {
+    test('getting random by breed with error', () async {
       when(dogsService.randomByBreed(breed)).thenThrow(Error());
 
       expect(() async => await repository.randomByBreed(breed),
@@ -65,6 +66,134 @@ void main() {
 
       verify(dogsService.randomByBreed(breed)).called(1);
       verifyNever(randomMapper.from(message));
+    });
+  });
+
+  group('dogs repository byBreed', () {
+    late List<String> message;
+    late String status;
+    late List<DogPic> pics;
+    late String breed;
+
+    setUp(() {
+      pics = DogPicFixture.factory().makeMany(3);
+      message = pics.map((e) => e.uri).toList(growable: false);
+      status = faker.randomGenerator.string(10);
+      breed = faker.randomGenerator.string(10);
+    });
+    test('getting by breed successfully', () async {
+      when(dogsService.byBreed(breed)).thenAnswer((_) async => Response(
+            message: message,
+            status: status,
+          ));
+      for (int i = 0; i < pics.length; i++) {
+        when(randomMapper.from(message[i])).thenReturn(pics[i]);
+      }
+
+      final result = await repository.byBreed(breed);
+
+      expect(result, pics);
+
+      verify(dogsService.byBreed(breed)).called(1);
+      for (int i = 0; i < pics.length; i++) {
+        verify(randomMapper.from(message[i])).called(1);
+      }
+    });
+
+    test('getting by breed with error', () async {
+      when(dogsService.byBreed(breed)).thenThrow(Error());
+
+      expect(() async => await repository.byBreed(breed),
+          throwsA(isA<RepositoryError>()));
+
+      verify(dogsService.byBreed(breed)).called(1);
+      verifyNever(randomMapper.from(any));
+    });
+  });
+
+  group('dogs repository randomBySubBreed', () {
+    late String message;
+    late String status;
+    late DogPic random;
+    late String breed;
+    late String subBreed;
+
+    setUp(() {
+      message = faker.internet.httpsUrl();
+      status = faker.randomGenerator.string(10);
+      random = DogPic(message);
+      breed = faker.randomGenerator.string(10);
+      subBreed = faker.randomGenerator.string(10);
+    });
+    test('getting random by sub-breed successfully', () async {
+      when(dogsService.randomBySubBreed(breed, subBreed))
+          .thenAnswer((_) async => Response(
+                message: message,
+                status: status,
+              ));
+      when(randomMapper.from(message)).thenReturn(random);
+
+      final result = await repository.randomBySubBreed(breed, subBreed);
+
+      expect(result, random);
+
+      verify(dogsService.randomBySubBreed(breed, subBreed)).called(1);
+      verify(randomMapper.from(message)).called(1);
+    });
+
+    test('getting random by sub-breed with error', () async {
+      when(dogsService.randomBySubBreed(breed, subBreed)).thenThrow(Error());
+
+      expect(() async => await repository.randomBySubBreed(breed, subBreed),
+          throwsA(isA<RepositoryError>()));
+
+      verify(dogsService.randomBySubBreed(breed, subBreed)).called(1);
+      verifyNever(randomMapper.from(message));
+    });
+  });
+
+  group('dogs repository bySubBreed', () {
+    late List<String> message;
+    late String status;
+    late List<DogPic> pics;
+    late String breed;
+    late String subBreed;
+
+    setUp(() {
+      pics = DogPicFixture.factory().makeMany(3);
+      message = pics.map((e) => e.uri).toList(growable: false);
+      status = faker.randomGenerator.string(10);
+      breed = faker.randomGenerator.string(10);
+      subBreed = faker.randomGenerator.string(10);
+    });
+    test('getting by sub-breed successfully', () async {
+      when(dogsService.bySubBreed(breed, subBreed))
+          .thenAnswer((_) async => Response(
+                message: message,
+                status: status,
+              ));
+      for (int i = 0; i < pics.length; i++) {
+        when(randomMapper.from(message[i])).thenReturn(pics[i]);
+      }
+
+      final result = await repository.bySubBreed(breed, subBreed);
+
+      expect(result, pics);
+
+      verify(dogsService.bySubBreed(breed, subBreed)).called(1);
+      for (int i = 0; i < pics.length; i++) {
+        verify(randomMapper.from(message[i])).called(1);
+      }
+    });
+
+    test('getting by sub-breed with error', () async {
+      when(dogsService.bySubBreed(breed, subBreed)).thenThrow(Error());
+
+      expect(() async => await repository.bySubBreed(breed, subBreed),
+          throwsA(isA<RepositoryError>()));
+
+      verify(dogsService.bySubBreed(breed, subBreed)).called(1);
+      verifyNever(randomMapper.from(any));
     });
   });
 }
