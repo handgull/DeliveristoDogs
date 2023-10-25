f20230cca67bffa0e612b008d6bc81fb95f46152
handgull <gullandrea693@gmail.com>
2023-10-25 19:29:35 +0200

    services tests done

--- test/services/network/breeds/breeds_service_test.dart
+++ test/services/network/breeds/breeds_service_test.dart
@@ -1,3 +1,5 @@
+import 'package:data_fixture_dart/data_fixture_dart.dart';
+import 'package:deliveristo_dogs/services/network/responses/all/all_response.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
@@ -12,10 +14,48 @@ void main() {
 
   setUp(() {
     dio = Dio(BaseOptions());
-    dioAdapter = DioAdapter(dio: dio);
+    dioAdapter = DioAdapter(
+      dio: dio,
+      matcher: const UrlRequestMatcher(),
+    );
 
     service = BreedsService(dio);
   });
 
-  //TODO: Test your methods
+  group('breeds service all', () {
+    late String status;
+    late AllResponse response;
+    late Map<String, Object> responseJson;
+    late Map<String, List<String>> message;
+
+    setUp(() {
+      message = {
+        "affenpinscher": [],
+        "australian": ["shepherd"],
+      };
+      status = faker.randomGenerator.string(10);
+      response = AllResponse(message: message, status: status);
+      responseJson = {'message': message, 'status': status};
+    });
+    test('getting all breeds successfully', () async {
+      dioAdapter.onGet(
+        '/breeds/list/all',
+        (server) => server.reply(200, responseJson),
+      );
+
+      expect(await service.all(), response);
+    });
+
+    test('getting all breeds with unexpected error', () async {
+      dioAdapter.onPatch(
+        '/breeds/list/all',
+        (server) => server.reply(500, null),
+      );
+
+      expect(
+        () async => await service.all(),
+        throwsException,
+      );
+    });
+  });
 }
--- test/services/network/dogs/dogs_service_test.dart
+++ test/services/network/dogs/dogs_service_test.dart
@@ -1,4 +1,6 @@
-import 'package:dio/dio.dart';
+import 'package:data_fixture_dart/data_fixture_dart.dart';
+import 'package:deliveristo_dogs/services/network/responses/response/response.dart';
+import 'package:dio/dio.dart' hide Response;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:deliveristo_dogs/services/network/dogs/dogs_service.dart';
@@ -12,10 +14,161 @@ void main() {
 
   setUp(() {
     dio = Dio(BaseOptions());
-    dioAdapter = DioAdapter(dio: dio);
+    dioAdapter = DioAdapter(
+      dio: dio,
+      matcher: const UrlRequestMatcher(),
+    );
 
     service = DogsService(dio);
   });
 
-  //TODO: Test your methods
+  group('dogs service randomByBreed', () {
+    late String status;
+    late Response<String> response;
+    late Map<String, Object> responseJson;
+    late String message;
+    late String breed;
+
+    setUp(() {
+      message = faker.randomGenerator.string(10);
+      status = faker.randomGenerator.string(10);
+      breed = faker.randomGenerator.string(10);
+      response = Response(message: message, status: status);
+      responseJson = {'message': message, 'status': status};
+    });
+    test('getting random successfully', () async {
+      dioAdapter.onGet(
+        '/breed/$breed/images/random',
+        (server) => server.reply(200, responseJson),
+      );
+
+      expect(await service.randomByBreed(breed), response);
+    });
+
+    test('getting random with unexpected error', () async {
+      dioAdapter.onPatch(
+        '/breed/$breed/images/random',
+        (server) => server.reply(500, null),
+      );
+
+      expect(
+        () async => await service.randomByBreed(breed),
+        throwsException,
+      );
+    });
+  });
+
+  group('dogs service byBreed', () {
+    late String status;
+    late Response<List<String>> response;
+    late Map<String, Object> responseJson;
+    late List<String> message;
+    late String breed;
+
+    setUp(() {
+      message = faker.randomGenerator
+          .amount((i) => faker.randomGenerator.string(10), 3);
+      status = faker.randomGenerator.string(10);
+      breed = faker.randomGenerator.string(10);
+      response = Response(message: message, status: status);
+      responseJson = {'message': message, 'status': status};
+    });
+    test('getting by breed successfully', () async {
+      dioAdapter.onGet(
+        '/breed/$breed/images',
+        (server) => server.reply(200, responseJson),
+      );
+
+      expect(await service.byBreed(breed), response);
+    });
+
+    test('getting by breed with unexpected error', () async {
+      dioAdapter.onPatch(
+        '/breed/$breed/images',
+        (server) => server.reply(500, null),
+      );
+
+      expect(
+        () async => await service.byBreed(breed),
+        throwsException,
+      );
+    });
+  });
+
+  group('dogs service randomBySubBreed', () {
+    late String status;
+    late Response<String> response;
+    late Map<String, Object> responseJson;
+    late String message;
+    late String breed;
+    late String subBreed;
+
+    setUp(() {
+      message = faker.randomGenerator.string(10);
+      status = faker.randomGenerator.string(10);
+      breed = faker.randomGenerator.string(10);
+      subBreed = faker.randomGenerator.string(10);
+      response = Response(message: message, status: status);
+      responseJson = {'message': message, 'status': status};
+    });
+    test('getting random successfully', () async {
+      dioAdapter.onGet(
+        '/breed/$breed/$subBreed/images/random',
+        (server) => server.reply(200, responseJson),
+      );
+
+      expect(await service.randomBySubBreed(breed, subBreed), response);
+    });
+
+    test('getting random with unexpected error', () async {
+      dioAdapter.onPatch(
+        '/breed/$breed/$subBreed/images/random',
+        (server) => server.reply(500, null),
+      );
+
+      expect(
+        () async => await service.randomBySubBreed(breed, subBreed),
+        throwsException,
+      );
+    });
+  });
+
+  group('dogs service bySubBreed', () {
+    late String status;
+    late Response<List<String>> response;
+    late Map<String, Object> responseJson;
+    late List<String> message;
+    late String breed;
+    late String subBreed;
+
+    setUp(() {
+      message = faker.randomGenerator
+          .amount((i) => faker.randomGenerator.string(10), 3);
+      status = faker.randomGenerator.string(10);
+      breed = faker.randomGenerator.string(10);
+      subBreed = faker.randomGenerator.string(10);
+      response = Response(message: message, status: status);
+      responseJson = {'message': message, 'status': status};
+    });
+    test('getting by sub-breed successfully', () async {
+      dioAdapter.onGet(
+        '/breed/$breed/$subBreed/images',
+        (server) => server.reply(200, responseJson),
+      );
+
+      expect(await service.bySubBreed(breed, subBreed), response);
+    });
+
+    test('getting by sub-breed with unexpected error', () async {
+      dioAdapter.onPatch(
+        '/breed/$breed/$subBreed/images',
+        (server) => server.reply(500, null),
+      );
+
+      expect(
+        () async => await service.bySubBreed(breed, subBreed),
+        throwsException,
+      );
+    });
+  });
 }
