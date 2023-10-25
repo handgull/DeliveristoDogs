20243b3a57152f72e1254eabeb84ab4c373cb36e
handgull <gullandrea693@gmail.com>
2023-10-25 09:33:40 +0200

    some temp UI

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -19,12 +19,27 @@ class DashboardPage extends StatelessWidget {
               Icons.shuffle,
             ),
             tooltip: context.t?.getRandom,
+          ),
+          IconButton(
+            onPressed: () async {
+              context.read<DogsBloc>().getByBreed('hound');
+            },
+            icon: const Icon(
+              Icons.search,
+            ),
+            tooltip: context.t?.getByBreed,
           )
         ],
       ),
       body: BlocConsumer<DogsBloc, DogsState>(
         listener: (BuildContext context, DogsState state) => state.whenOrNull(
           errorGettingRandom: (error) =>
+              ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(error.localizedString(context) ?? error.toString()),
+            ),
+          ),
+          errorGettingByBreed: (error) =>
               ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
               content: Text(error.localizedString(context) ?? error.toString()),
@@ -41,7 +56,22 @@ class DashboardPage extends StatelessWidget {
               gettingRandom: () => const Center(
                 child: CircularProgressIndicator(),
               ),
+              gettingByBreed: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
               gettedRandom: (random) => Image.network(random.uri),
+              gettedByBreed: (pics) => GridView.builder(
+                  padding: const EdgeInsets.all(12),
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 12,
+                    mainAxisSpacing: 12,
+                  ),
+                  itemCount: pics.length,
+                  itemBuilder: (BuildContext context, index) {
+                    return Image.network(pics[index].uri);
+                  }),
             ) ??
             const SizedBox.shrink(),
       ),
